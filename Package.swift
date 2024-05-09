222218f5ccb3a3369b933ca201ddbe0b06afc588
Dmytro Lunov <dima.l@wearepush.co>
2024-05-09 11:01:10 +0300

    staq wallet

---
+++ Package.swift
@@ -0,0 +1,20 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "StaqWallet",
+    platforms: [
+        .iOS(.v15),
+    ],
+    products: [
+        .library(name: "StaqWallet", targets: ["StaqWallet"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/staqio/TrustlessSDK", from: "0.2.1"),
+    ],
+    targets: [
+        .binaryTarget(name: "StaqWallet", path: "./Sources/StaqWallet.xcframework"),
+    ]
+)
---
+++ Sources/StaqWallet.xcframework/ios-arm64_x86_64-simulator/StaqWallet.framework/Headers/StaqWallet.h
@@ -0,0 +1,17 @@
+//
+//  StaqWallet.h
+//  StaqWallet
+//
+//  Created by Dmytro Lunov on 25.04.2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for StaqWallet.
+FOUNDATION_EXPORT double StaqWalletVersionNumber;
+
+//! Project version string for StaqWallet.
+FOUNDATION_EXPORT const unsigned char StaqWalletVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StaqWallet/PublicHeader.h>
+
